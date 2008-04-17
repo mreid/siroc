85fb7a4be3d51a12044c26f7f58db38c771e3d19
Mark Reid <mark@threewordslong.com>
2008-04-17 13:40:03 +1000

    Made pretty. Fixed SI -> ROC conversion bug. Added axes and titles.

--- applet/PlotView.pde
+++ applet/PlotView.pde
@@ -1,11 +1,30 @@
 import java.util.Vector;
 
+class CurveView {
+  GCurve curve;
+  color  col;  
+  CurveView(GCurve curve, color col) { this.curve = curve; this.col = col; }
+}
+
+class LineView {
+  GLine line;
+  color  col;  
+  LineView(GLine line, color col) { this.line = line; this.col = col; }
+}
+
+class PointView {
+  GPoint point;
+  color  col;  
+  PointView(GPoint point, color col) { this.point = point; this.col = col; }
+}
+
 class PlotView {
 
   Vector curves = new Vector();
-  Vector points = new Vector();
+  Vector points = new Vector();  
   Vector lines  = new Vector();
   
+  String title = "[Plot Title]";
   int vxStart, vyStart;
   int vxEnd, vyEnd;
   float xStart, xEnd;
@@ -14,39 +33,27 @@ class PlotView {
   GBounds bounds;
   
   PlotView(
-    int vx0, int vy0, int vx1, int vy1,
+    int vx0, int vy0, int vWidth, int vHeight,
     float x0, float y0, float x1, float y1
   ) {
-    vxStart = vx0; vyStart = vy0; vxEnd = vx1; vyEnd = vy1;
+    vxStart = vx0; vyStart = vy0; vxEnd = vx0 + vWidth; vyEnd = vy0 + vHeight;
     xStart = x0; yStart = y0; xEnd = x1; yEnd = y1;
     bounds = new GBounds(xStart, yStart, xEnd, yEnd);
   }
 
-  PlotView(float x0, float y0, float x1, float y1) {
-   this(0, 0, 100, 100, x0, y0, x1, y1); 
-  }
+  PlotView(float x0, float y0, float x1, float y1) { this(0, 0, 100, 100, x0, y0, x1, y1); }
+  PlotView() { this(0.0, 0.0, 1.0, 1.0); }
   
-  PlotView() {
-    this(0.0, 0.0, 1.0, 1.0);
-  }
+  void add(GCurve curve, color col) { curves.add(new CurveView(curve, col)); }
+  void add(GPoint point, color col) { points.add(new PointView(point, col)); }
+  void add(GLine line, color col) { lines.add(new LineView(line, col)); }
   
-  void add(GCurve curve) {
-    curves.add(curve); 
-  }
-
-  void add(GPoint point) {
-    points.add(point);  
-  }
-  
-  void add(GLine line) {
-     lines.add(line); 
-  }
-  
+  void setTitle(String title) { this.title = title; }
   void setViewStart(int vx, int vy) { vxStart = vx; vyStart = vy; }
   void setViewEnd(int vx, int vy) { vxEnd = vx; vyEnd = vy; }
-  void setView(int vx0, int vy0, int vx1, int vy1) { 
+  void setView(int vx0, int vy0, int vWidth, int vHeight) { 
     setViewStart(vx0, vy0); 
-    setViewEnd(vx1, vy1); 
+    setViewEnd(vx0 + vWidth, vHeight); 
   }
   
   int viewWidth() { return vxEnd - vxStart; }
@@ -55,23 +62,19 @@ class PlotView {
   float xRange() { return xEnd - xStart; }
   float yRange() { return yEnd - yStart; }
   
+  float xResolution() { return xRange() / viewWidth(); }
+  float yResolution() { return yRange() / viewHeight(); }
+  
   float viewToModelX(float vx) { return xStart + xRange() * (vx - vxStart) / viewWidth(); }
   float viewToModelY(float vy) { return yStart - yRange() * (vy - vyEnd) / viewHeight(); }
   
    boolean active() {
      return (mouseX > vxStart && mouseX < vxEnd && mouseY > vyStart && mouseY < vyEnd);
    }
-  
-   void showCursor() {
-    /*
-     RShape c = new RShape();
-     c.addMoveTo(viewToModelX(mouseX), 0.0);
-     c.addLineTo(viewToModelX(mouseX), 1.0);
-     display(c, g);
-    */
-   }
-  
-   void draw(PGraphics g) {
+    
+   void preDraw() {
+     drawTitle();
+     drawBounds();
      
      pushMatrix();
      
@@ -80,7 +83,41 @@ class PlotView {
      translate(-xStart, -yStart);
      scale(viewWidth()/xRange(),-viewHeight()/yRange());
      translate(0,-yEnd);
+     strokeWeight(min(xResolution(), yResolution()));     
+   }
+   
+   void postDraw() {
+     popMatrix(); 
+   }
+  
+   void drawTitle() {
+     fill(0);
+     textFont(titleFont);
+     
+     textAlign(CENTER, BOTTOM);
+     text(title, vxStart + viewWidth()/2, vyStart - 1);
+   }
+  
+   void drawBounds() {
+     fill(0);
+     textFont(tickFont);
 
+     textAlign(LEFT, TOP);
+     text(nf(xStart, 1, 1), vxStart, vyEnd + 1);
+
+     textAlign(RIGHT, TOP);
+     text(nf(xEnd, 1, 1), vxEnd, vyEnd + 1);
+     
+     textAlign(RIGHT, BOTTOM);
+     text(nf(yStart, 1, 1), vxStart - 1, vyEnd);     
+
+     textAlign(RIGHT, TOP);
+     text(nf(yEnd, 1, 1), vxStart - 1, vyStart);     
+   }
+  
+   void draw(PGraphics g) {
+     preDraw();
+     
      // Outline
      noFill();
      stroke(200);
@@ -88,23 +125,38 @@ class PlotView {
 
      // Curves
      for(int i = 0 ; i < curves.size() ; i++) {
-       stroke(0);
-       viewCurve((GCurve) curves.get(i)).draw(g);
+       CurveView v = (CurveView) curves.get(i);
+       stroke(v.col);
+       viewCurve(v.curve).draw(g);
      }
      
      // Points
      for(int i = 0 ; i < points.size() ; i++) {
-       stroke(200,0,0);
-       viewPoint((GPoint) points.get(i));
+       PointView v = (PointView) points.get(i);
+       stroke(v.col);
+       viewPoint(v.point);
      }
      
      // Lines
      for(int i = 0 ; i < lines.size() ; i++) {
-       stroke(200,0,0);
-       viewLine((GLine) lines.get(i)).draw(g);
+       LineView v = (LineView) lines.get(i);
+       stroke(v.col);
+       viewLine(v.line).draw(g);
      }
      
-     popMatrix();
+     postDraw();
+   }
+   
+   void drawPoint(GPoint p) {
+     preDraw();
+     viewPoint(p);
+     postDraw();     
+   }
+   
+   void drawLine(GLine l) {
+     preDraw();
+     viewLine(l).draw(g);
+     postDraw(); 
    }
    
    RContour viewCurve(GCurve curve) {
@@ -118,7 +170,14 @@ class PlotView {
    }
    
    void viewPoint(GPoint point) {
-     point(point.getX(), point.getY());
+     float x = point.getX();
+     float y = point.getY();
+     
+     float xRes = 2*xResolution();
+     float yRes = 2*yResolution();
+     
+     line(x - xRes, y - yRes, x + xRes, y + yRes);
+     line(x + yRes, y - yRes, x - xRes, y + yRes);
    }
    
    RContour viewLine(GLine line) {
@@ -126,8 +185,6 @@ class PlotView {
      RContour l = new RContour();
      l.addPoint(s.start.getX(), s.start.getY());
      l.addPoint(s.end.getX(), s.end.getY());
-//     l.addPoint(0.0, line.atX(0.0));
-//     l.addPoint(1.0, line.atX(1.0));
      return l;
    }
 }
--- applet/siroc.java
+++ applet/siroc.java
@@ -1,37 +1,53 @@
-import processing.core.*; import geomerative.*; import name.reid.mark.geovex.*; import java.util.Vector; import java.applet.*; import java.awt.*; import java.awt.image.*; import java.awt.event.*; import java.io.*; import java.net.*; import java.text.*; import java.util.*; import java.util.zip.*; import javax.sound.midi.*; import javax.sound.midi.spi.*; import javax.sound.sampled.*; import javax.sound.sampled.spi.*; import java.util.regex.*; import javax.xml.parsers.*; import javax.xml.transform.*; import javax.xml.transform.dom.*; import javax.xml.transform.sax.*; import javax.xml.transform.stream.*; import org.xml.sax.*; import org.xml.sax.ext.*; import org.xml.sax.helpers.*; public class siroc extends PApplet {/**
+import processing.core.*; import controlP5.*; import geomerative.*; import name.reid.mark.geovex.*; import java.util.Vector; import java.applet.*; import java.awt.*; import java.awt.image.*; import java.awt.event.*; import java.io.*; import java.net.*; import java.text.*; import java.util.*; import java.util.zip.*; import javax.sound.midi.*; import javax.sound.midi.spi.*; import javax.sound.sampled.*; import javax.sound.sampled.spi.*; import java.util.regex.*; import javax.xml.parsers.*; import javax.xml.transform.*; import javax.xml.transform.dom.*; import javax.xml.transform.sax.*; import javax.xml.transform.stream.*; import org.xml.sax.*; import org.xml.sax.ext.*; import org.xml.sax.helpers.*; public class siroc extends PApplet {
+
+
+
+/**
  * <p>
  * Visualisation of the relationship between Statistical Information
  * and ROC curves.
  * </p><p>
- * The left graph shows two curves on a Statistical Information Graph.
+ * The left graph shows curves on a Statistical Information plot.
  * The right graph shows the same curves converted to an ROC representation
  * with \u03c0 = 0.5.
  * </p><p>
- * Move your mouse over the S.I. graph to see the corresponding line in the
- * ROC graph.
+ * Move your mouse over the S.I. window to see the corresponding line in the
+ * ROC graph and <i>vice versa</i>.
+ * </p>
+ * <p>
+ * The duality relationships are computed using the 
+ * <a href="http://gihub.com/mreid/geovex">geovex</a> Java library while the visualisation
+ * is done with the standard Processing library and the 
+ * <a href="http://www.ricardmarxer.com/processing/geomerative/documentation/">geomerative</a> 
+ * library.
  * </p>
  */
-
-
-
 PlotView rocView = new PlotView(0.0f, 0.0f, 1.0f, 1.0f);
-PlotView siView  = new PlotView(0.0f, 0.0f, 1.0f, 0.25f);
+PlotView siView  = new PlotView(0.0f, 0.0f, 1.0f, 0.5f);
 
-Converter sirocConvert = new SIROCConverter();
-Converter rocsiConvert = new ROCSIConverter();
+SIROCConverter sirocConvert = new SIROCConverter();
+ROCSIConverter rocsiConvert = new ROCSIConverter();
 
 SpecPoint siCursor = new SpecPoint(0.0f, 0.0f);
+SpecPoint rocCursor = new SpecPoint(0.0f, 0.0f);
+GLine rocDualCursor = new DualLine(siCursor, sirocConvert);
+GLine siDualCursor = new DualLine(rocCursor, rocsiConvert); 
+
+PFont titleFont = createFont("Arial", 16);
+PFont tickFont  = createFont("Arial", 12);
+
+float prior = 0.5f;
 
 public void setup(){
-  size(700,400,P3D);
+  size(700,400,JAVA2D);
   frameRate(20);
   background(255);
-  fill(0);
-  //noFill();
-  stroke(255,0,0);
 
-  siView.setView(10, 10, 300, 300);
-  rocView.setView(310, 10, 610, 300);
+  siView.setView(30, 30, 300, 300);
+  siView.setTitle("Stat. Info. (\u03c0 = 0.5)");
+  
+  rocView.setView(360, 30, 300, 300);
+  rocView.setTitle("ROC");
 
   SpecCurve siTent = new SpecCurve();
   siTent.add(0.0f, 0.0f);
@@ -45,18 +61,32 @@ public void setup(){
   si.add(0.75f, 0.125f);
   si.add(1.0f, 0.0f);
 
-  siView.add(siTent);
-  siView.add(si);
+  int grey = color(160,160,160);
+  int black = color(0,0,0);
 
-  rocView.add(sirocConvert.toCurve(siTent));
-  rocView.add(sirocConvert.toCurve(si));
+  siView.add(siTent, grey);
+  siView.add(si, black);
 
-  siView.add(siCursor);
-  rocView.add(new DualLine(siCursor, sirocConvert));
+  rocView.add(sirocConvert.toCurve(siTent), grey);
+  rocView.add(sirocConvert.toCurve(si), black);  
+
+  // Controls for prior value
+//  ControlP5 priorControl = new ControlP5(this);
+//  Slider s = priorControl.addSlider("priorSlider", 0.0, 1.0, 0.5, 250, 350, 200, 20);
+
+  smooth();
 }
 
+/*
+void priorSlider(float value) {
+  prior = value;
+  sirocConvert.setPrior(prior);
+  rocsiConvert.setPrior(prior);
+}
+*/
 
 public void draw(){
+  // Clear the screen
   background(255);
   
   siView.draw(g);
@@ -65,17 +95,49 @@ public void draw(){
   if(siView.active()) {
     siCursor.setX(siView.viewToModelX(mouseX));
     siCursor.setY(siView.viewToModelY(mouseY));
+    
+    stroke(200,0,0);
+    siView.drawPoint(siCursor);
+    rocView.drawLine(rocDualCursor);
+  }
+  
+  if(rocView.active()) {
+    rocCursor.setX(rocView.viewToModelX(mouseX));
+    rocCursor.setY(rocView.viewToModelY(mouseY)); 
+
+    stroke(0,0,200);    
+    rocView.drawPoint(rocCursor);
+    siView.drawLine(siDualCursor);
   }
 }
 
 
 
+class CurveView {
+  GCurve curve;
+  int  col;  
+  CurveView(GCurve curve, int col) { this.curve = curve; this.col = col; }
+}
+
+class LineView {
+  GLine line;
+  int  col;  
+  LineView(GLine line, int col) { this.line = line; this.col = col; }
+}
+
+class PointView {
+  GPoint point;
+  int  col;  
+  PointView(GPoint point, int col) { this.point = point; this.col = col; }
+}
+
 class PlotView {
 
   Vector curves = new Vector();
-  Vector points = new Vector();
+  Vector points = new Vector();  
   Vector lines  = new Vector();
   
+  String title = "[Plot Title]";
   int vxStart, vyStart;
   int vxEnd, vyEnd;
   float xStart, xEnd;
@@ -84,39 +146,27 @@ class PlotView {
   GBounds bounds;
   
   PlotView(
-    int vx0, int vy0, int vx1, int vy1,
+    int vx0, int vy0, int vWidth, int vHeight,
     float x0, float y0, float x1, float y1
   ) {
-    vxStart = vx0; vyStart = vy0; vxEnd = vx1; vyEnd = vy1;
+    vxStart = vx0; vyStart = vy0; vxEnd = vx0 + vWidth; vyEnd = vy0 + vHeight;
     xStart = x0; yStart = y0; xEnd = x1; yEnd = y1;
     bounds = new GBounds(xStart, yStart, xEnd, yEnd);
   }
 
-  PlotView(float x0, float y0, float x1, float y1) {
-   this(0, 0, 100, 100, x0, y0, x1, y1); 
-  }
+  PlotView(float x0, float y0, float x1, float y1) { this(0, 0, 100, 100, x0, y0, x1, y1); }
+  PlotView() { this(0.0f, 0.0f, 1.0f, 1.0f); }
   
-  PlotView() {
-    this(0.0f, 0.0f, 1.0f, 1.0f);
-  }
+  public void add(GCurve curve, int col) { curves.add(new CurveView(curve, col)); }
+  public void add(GPoint point, int col) { points.add(new PointView(point, col)); }
+  public void add(GLine line, int col) { lines.add(new LineView(line, col)); }
   
-  public void add(GCurve curve) {
-    curves.add(curve); 
-  }
-
-  public void add(GPoint point) {
-    points.add(point);  
-  }
-  
-  public void add(GLine line) {
-     lines.add(line); 
-  }
-  
+  public void setTitle(String title) { this.title = title; }
   public void setViewStart(int vx, int vy) { vxStart = vx; vyStart = vy; }
   public void setViewEnd(int vx, int vy) { vxEnd = vx; vyEnd = vy; }
-  public void setView(int vx0, int vy0, int vx1, int vy1) { 
+  public void setView(int vx0, int vy0, int vWidth, int vHeight) { 
     setViewStart(vx0, vy0); 
-    setViewEnd(vx1, vy1); 
+    setViewEnd(vx0 + vWidth, vHeight); 
   }
   
   public int viewWidth() { return vxEnd - vxStart; }
@@ -125,23 +175,19 @@ class PlotView {
   public float xRange() { return xEnd - xStart; }
   public float yRange() { return yEnd - yStart; }
   
+  public float xResolution() { return xRange() / viewWidth(); }
+  public float yResolution() { return yRange() / viewHeight(); }
+  
   public float viewToModelX(float vx) { return xStart + xRange() * (vx - vxStart) / viewWidth(); }
   public float viewToModelY(float vy) { return yStart - yRange() * (vy - vyEnd) / viewHeight(); }
   
    public boolean active() {
      return (mouseX > vxStart && mouseX < vxEnd && mouseY > vyStart && mouseY < vyEnd);
    }
-  
-   public void showCursor() {
-    /*
-     RShape c = new RShape();
-     c.addMoveTo(viewToModelX(mouseX), 0.0);
-     c.addLineTo(viewToModelX(mouseX), 1.0);
-     display(c, g);
-    */
-   }
-  
-   public void draw(PGraphics g) {
+    
+   public void preDraw() {
+     drawTitle();
+     drawBounds();
      
      pushMatrix();
      
@@ -150,7 +196,41 @@ class PlotView {
      translate(-xStart, -yStart);
      scale(viewWidth()/xRange(),-viewHeight()/yRange());
      translate(0,-yEnd);
+     strokeWeight(min(xResolution(), yResolution()));     
+   }
+   
+   public void postDraw() {
+     popMatrix(); 
+   }
+  
+   public void drawTitle() {
+     fill(0);
+     textFont(titleFont);
+     
+     textAlign(CENTER, BOTTOM);
+     text(title, vxStart + viewWidth()/2, vyStart - 1);
+   }
+  
+   public void drawBounds() {
+     fill(0);
+     textFont(tickFont);
 
+     textAlign(LEFT, TOP);
+     text(nf(xStart, 1, 1), vxStart, vyEnd + 1);
+
+     textAlign(RIGHT, TOP);
+     text(nf(xEnd, 1, 1), vxEnd, vyEnd + 1);
+     
+     textAlign(RIGHT, BOTTOM);
+     text(nf(yStart, 1, 1), vxStart - 1, vyEnd);     
+
+     textAlign(RIGHT, TOP);
+     text(nf(yEnd, 1, 1), vxStart - 1, vyStart);     
+   }
+  
+   public void draw(PGraphics g) {
+     preDraw();
+     
      // Outline
      noFill();
      stroke(200);
@@ -158,23 +238,38 @@ class PlotView {
 
      // Curves
      for(int i = 0 ; i < curves.size() ; i++) {
-       stroke(0);
-       viewCurve((GCurve) curves.get(i)).draw(g);
+       CurveView v = (CurveView) curves.get(i);
+       stroke(v.col);
+       viewCurve(v.curve).draw(g);
      }
      
      // Points
      for(int i = 0 ; i < points.size() ; i++) {
-       stroke(200,0,0);
-       viewPoint((GPoint) points.get(i));
+       PointView v = (PointView) points.get(i);
+       stroke(v.col);
+       viewPoint(v.point);
      }
      
      // Lines
      for(int i = 0 ; i < lines.size() ; i++) {
-       stroke(200,0,0);
-       viewLine((GLine) lines.get(i)).draw(g);
+       LineView v = (LineView) lines.get(i);
+       stroke(v.col);
+       viewLine(v.line).draw(g);
      }
      
-     popMatrix();
+     postDraw();
+   }
+   
+   public void drawPoint(GPoint p) {
+     preDraw();
+     viewPoint(p);
+     postDraw();     
+   }
+   
+   public void drawLine(GLine l) {
+     preDraw();
+     viewLine(l).draw(g);
+     postDraw(); 
    }
    
    public RContour viewCurve(GCurve curve) {
@@ -188,7 +283,14 @@ class PlotView {
    }
    
    public void viewPoint(GPoint point) {
-     point(point.getX(), point.getY());
+     float x = point.getX();
+     float y = point.getY();
+     
+     float xRes = 2*xResolution();
+     float yRes = 2*yResolution();
+     
+     line(x - xRes, y - yRes, x + xRes, y + yRes);
+     line(x + yRes, y - yRes, x - xRes, y + yRes);
    }
    
    public RContour viewLine(GLine line) {
@@ -196,8 +298,6 @@ class PlotView {
      RContour l = new RContour();
      l.addPoint(s.start.getX(), s.start.getY());
      l.addPoint(s.end.getX(), s.end.getY());
-//     l.addPoint(0.0, line.atX(0.0));
-//     l.addPoint(1.0, line.atX(1.0));
      return l;
    }
 }
